f941e59c06f9362d84f9c410ac8289d579b39bd9
lo-ivan <130327199+lo-ivan@users.noreply.github.com>
2023-12-08 19:56:45 -0800

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,3 @@
 //const apiLink = 'http://localhost:3001/book';
-const apiLink = 'https://final-exam-backend.onrender.com';
+const apiLink = 'https://final-exam-backend.onrender.com/book';
 module.exports = apiLink
